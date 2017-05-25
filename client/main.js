a5c0ef79b4fb32ef285ec0fddafd9184573c69fd
Michelangelo <michelangelo.ph@gmail.com>
2017-05-24 19:38:16 -0700

    Bootstrap and navbar added

--- client/main.js
+++ client/main.js
@@ -1,16 +1,5 @@
-Meteor.setInterval(function(){
-	Session.set("current_date", new Date());
-}, 1000);
-
 Template.editor.helpers({
   docid:function(){
 		return Documents.findOne()._id;
 	}
 });
-
-Template.date_display.helpers({
-  "current_date":function(){
-		return Session.get("current_date");
-	}
-});
-
