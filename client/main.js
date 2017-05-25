fddd16c3d3409c3b03d3abc21a87790734a4dfa1
Michelangelo <michelangelo.ph@gmail.com>
2017-05-25 10:41:04 -0700

    Live preview

--- client/main.js
+++ client/main.js
@@ -1,5 +1,13 @@
 Template.editor.helpers({
   docid:function(){
 		return Documents.findOne()._id;
+	},
+	config:function(){
+		return function(editor){
+			editor.on("change", function(cm_editor, info){
+				console.log(cm_editor.getValue());
+				$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+			});
+		}
 	}
 });
