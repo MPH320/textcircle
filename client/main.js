64849af2e6779e9eed77ef328b6b8e6a10a4f4f1
Michelangelo <michelangelo.ph@gmail.com>
2017-06-02 16:05:33 -0700

    Added username field to sign up form

--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
+Accounts.ui.config({
+		passwordSignupFields: "USERNAME_AND_EMAIL"
+	});
+
 Template.editor.helpers({
   docid:function(){
 		return Documents.findOne()._id;
@@ -5,7 +9,7 @@ Template.editor.helpers({
 	config:function(){
 		return function(editor){
 			editor.on("change", function(cm_editor, info){
-				console.log(cm_editor.getValue());
+			
 				$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
 				Meteor.call("addEditingUser")
 			});
@@ -19,23 +23,17 @@ Template.editingUsers.helpers({
 		doc = Documents.findOne();
 		if (!doc) {return;}
 		eusers = EditingUsers.findOne({docid:doc._id});
+		
 		if (!eusers) {return;}
 		users = new Array();
 		var i = 0;
 		for (var user_id in eusers.users) {
-			console.log(eusers.users[user_id]);
-			users[i] = fixObjectKeys(eusers.users[user_id]);
+			
+			users[i] = eusers.users[user_id];
+			
 			i++;
 		}
 		return users;
 	}
 });
 
-var fixObjectKeys = function(obj) {
-	var newObj = {};
-	for (key in obj) {
-		var key2 = key.replace("-", "");
-		newObj[key2] = obj[key];
-	}
-	return newObj;
-}
